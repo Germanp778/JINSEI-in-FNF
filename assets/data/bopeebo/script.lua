c92ada14380febc2616641dc6ee85460317527e0
Germanp778 <87247940+Germanp778@users.noreply.github.com>
2025-05-10 11:51:32 -0400

    JINSEI

--- assets/data/bopeebo/script.lua
+++ assets/data/bopeebo/script.lua
@@ -6,13 +6,8 @@ function onBeatHit()
     --CAMERA STUFF
 
     -- tilt and zoom start 1
-    if curBeat == 1 then
+    if curBeat > 1 then
         doTweenAngle('screenTilt', 'camGame', 5, 8, 'quadInOut');
         doTweenZoom('screenZoom', 'camGame', 0.8, 8, 'quadInOut');
-    end
-        -- tilt and zoom end 1
-     if curBeat == 64 then
-        doTweenAngle('screenTilt', 'camGame', 0, 1.5, 'backOut');
-        doTweenZoom('screenZoom', 'camGame', 0.7, 1, 'quadInOut');
     end
 end
